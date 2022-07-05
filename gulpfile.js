6132aba2d77892aeabd7e5112e6cd7276f12d42c
brianalvesm <hey@brianmoreira.com>
2022-07-04 22:39:08 -0300

    Move arquivos do public para o root

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ var sass = require('gulp-sass')(require('sass'));
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./public/sass/**/*.sass')
+  return gulp.src('./sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/stylesheets/'));
+    .pipe(gulp.dest('./stylesheets/'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./public/sass/**/*.sass', gulp.series('sass'));
+  gulp.watch('./sass/**/*.sass', gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('watch', 'sass'));
