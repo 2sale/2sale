45efa0c9b0d235e678f001f7f77bc89e201005c7
brianalvesm <hey@brianmoreira.com>
2022-07-05 13:23:36 -0300

    Cria script para comportamento do componente

---
+++ javascripts/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	(function ($) {
+
+	  $('.model-item').click( function() {
+	  	var $filterName = ($(this).data('model'))
+	  	var $filtered = $('[data-model-target=' + $filterName + ']')
+	  	var $allTargets = $('[data-model-target]')
+			
+			$(this).addClass('active');
+	  	$(this).siblings().removeClass('active');
+	  	$allTargets.hide();
+	  	$filtered.show();
+
+		});
+
+	})(jQuery);
+});
